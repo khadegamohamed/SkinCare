edd5167be55aee97b3a8c2a6b8fe79d9ddfc0a39
khadegamohamed <khadegamohamedsaed@gmail.com>
2021-04-27 01:49:54 +0200

    hello

---
+++ app/src/main/java/com/example/skincare/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.skincare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this,"welcome",Toast.LENGTH_LONG).show()
+    }
+}

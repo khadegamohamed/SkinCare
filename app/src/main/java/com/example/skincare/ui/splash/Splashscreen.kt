e4453a6c907bed8fd84cd5880962e054c6d022f6
zienabhussien <zienabh08@gmail.com>
2021-05-02 17:57:35 +0200

    ..

--- app/src/main/java/com/example/skincare/ui/splash/Splashscreen.kt
+++ app/src/main/java/com/example/skincare/ui/splash/Splashscreen.kt
@@ -1,9 +1,11 @@
-package com.example.skincare
+package com.example.skincare.ui.splash
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.example.skincare.MainActivity
+import com.example.skincare.R
 
 class splashscreen : AppCompatActivity() {
 
@@ -11,7 +13,7 @@ class splashscreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
         Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         },3000)
     }
